0bc87c784a287de90801713907ee1a03d7ed14ca
LakshitXD <lakshitk6666@gmail.com>
2021-09-30 16:28:15 +0530

    basic boilerplate

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="border-t border-gray-400 text-center py-6">
+      <a
+        href="https://github.com/ezpz-coders"
+        target="_blank"
+        rel="noopener noreferrer">
+        YOLO
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
